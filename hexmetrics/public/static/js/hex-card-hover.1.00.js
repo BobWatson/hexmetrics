fbcbf215445a4832e290f66cf67c9df6a6619e25
Bob Watson <bob@nearimpossible.com>
2013-07-06 12:58:59 +1000

    Some adjustments to the javascript to cope with fast mousein/outs

--- hexmetrics/public/static/js/hex-card-hover.1.00.js
+++ hexmetrics/public/static/js/hex-card-hover.1.00.js
@@ -1,14 +1,12 @@
 $(document).ready (function () {
 
-
 	$("body").append('<div style="display:none;" id="hex-cards-box"><iframe scrolling="no" style=""></iframe></div>')
 	
 	$('#hex-cards-box iframe').ready( function () {
-		$('#hex-cards-box').css('width', '250px');
-		$('#hex-cards-box').css('height', '350px');
+		$('#hex-cards-box').css('width', '251px');
+		$('#hex-cards-box').css('height', '351px');
 		$('#hex-cards-box').css('margin', '0');
 		$('#hex-cards-box').css('padding', '0');
-		$('#hex-cards-box').css('background-color', '#000');
 		
 		$('#hex-cards-box iframe').css('width', '250px');
 		$('#hex-cards-box iframe').css('height', '350px');
@@ -18,6 +16,8 @@ $(document).ready (function () {
 		$('#hex-cards-box iframe').css('padding', '0');
 	})
 	
+	$should_show_hex = true;
+	
 	$("a.hex-card").each(function () {
 		$(this).hover( function (e) {
 			$a_ref = $(this);
@@ -26,15 +26,17 @@ $(document).ready (function () {
 	        $this.removeAttr('title');
 			$(document).bind('mousemove', function(e){
 		    	$('#hex-cards-box').offset({left:e.pageX+20,top:e.pageY+10});
+		    	if($should_show_hex == false) $("#hex-cards-box").hide();
 			});
 			
-			
+			$should_show_hex = true;
 			$("#hex-cards-box iframe").prop("src", 'http://hexmetrics.ni.tl/tools/cardJSFrame?url='+$a_ref.prop('href'));
 			$("#hex-cards-box iframe").load ( function () {
-				$("#hex-cards-box").show()
+				if($should_show_hex == true) $("#hex-cards-box").show();
 			})
 					
 		}, function () {
+			$should_show_hex = false;
 			$("#hex-cards-box").hide()
 			$this = $(this);
 	        $this.attr('title', $this.data('title'));
